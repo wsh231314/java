8a62caf0a34010c073d735e41d6c370c9217955a
shawn.shaohua.wang <shawn.shaohua.wang@CDC108203.dir.svc.accenture.com>
2016-09-12 09:36:10 +0800

    server
    Change-Id: Id21c70851055673f26c56a911cfbb89aaa70c513
    Signed-off-by: shawn.shaohua.wang <shawn.shaohua.wang@CDC108203.dir.svc.accenture.com>

---
+++ src/java_training/co/jp/collection/ListTest.java
@@ -0,0 +1,10 @@
+package java_training.co.jp.collection;
+
+public class ListTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/java_training/co/jp/io/FileTest.java
@@ -0,0 +1,5 @@
+package java_training.co.jp.io;
+
+public class FileTest {
+
+}
